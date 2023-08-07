d605058b698f2061028c614a8a54ecf4bd6c6b49
madhavi <madhavilatha.008@gmail.com>
2023-08-07 15:06:32 -0500

    pipeline

--- redis.sh
+++ redis.sh
@@ -1,5 +1,6 @@
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-yum module enable redis:remi-6.2 -y
+dnf module enable redis:remi-6.2 -y
 yum install redis -y
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
 systemctl enable redis
-systemctl start redis
+systemctl restart redis
