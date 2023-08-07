876007f90acc4a463a98c49f41cf3d20372a9eb2
madhavi <madhavilatha.008@gmail.com>
2023-08-07 13:20:38 -0500

    pipeline

--- catalogue.sh
+++ catalogue.sh
@@ -22,7 +22,7 @@ echo -e "\e[36m>>>>> Add NodeJS Dependencies <<<<<\e[0m"
 npm install
 
 echo -e "\e[36m>>>>> Copy Catalogue SystemD file <<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
 
 echo -e "\e[36m>>>>> Start Catalogue Service <<<<<\e[0m"
 systemctl daemon-reload
@@ -30,7 +30,7 @@ systemctl enable catalogue
 systemctl restart catalogue
 
 echo -e "\e[36m>>>>> Copy Mongodb repo <<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
 
 echo -e "\e[36m>>>>> Install Mongodb Client <<<<<\e[0m"
 yum install mongodb-org-shell -y
