c7cd32afc6215fc20a8679be2e7350f05d3c20e9
madhavi <madhavilatha.008@gmail.com>
2023-08-07 12:49:55 -0500

    pipeline

--- catalogue.sh
+++ catalogue.sh
@@ -13,4 +13,4 @@ systemctl enable catalogue
 systemctl start catalogue
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org-shell -y
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+mongo --host mongodb-dev.madhavi91.online </app/schema/catalogue.js
