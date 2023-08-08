8d45f5f5a8f1796ba992046604cd24438d657e58
madhavi <madhavilatha.008@gmail.com>
2023-08-08 13:23:04 -0500

    pipeline

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
-yum module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+dnf module disable mysql -y
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
 yum install mysql-community-server -y
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
 mysql_secure_installation --set-root-pass RoboShop@1
 mysql -uroot -pRoboShop@1
