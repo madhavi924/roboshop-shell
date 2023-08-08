848d1f5b47d837728d34680a9eba5601ab8b84ee
madhavi <madhavilatha.008@gmail.com>
2023-08-08 14:53:02 -0500

    pipeline

--- payment.sh
+++ payment.sh
@@ -4,9 +4,8 @@ mkdir /app
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 cd /app
 unzip /tmp/payment.zip
-cd /app
 pip3.6 install -r requirements.txt
-cp payment.service /etc/systemd/system/payment.service
+cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service
 systemctl daemon-reload
 systemctl enable payment
-systemctl start payment
+systemctl restart payment
