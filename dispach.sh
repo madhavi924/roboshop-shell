446d0c62a2333888bfc583f1e32c22dad96ed755
madhavi <madhavilatha.008@gmail.com>
2023-08-08 14:55:29 -0500

    pipeline

--- dispach.sh
+++ dispach.sh
@@ -1,14 +1,14 @@
 yum install golang -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
 cd /app
 unzip /tmp/dispatch.zip
-cd /app
 go mod init dispatch
 go get
 go build
-cp dispatch.service /etc/systemd/system/dispatch.service
+cp /home/centos/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service
 systemctl daemon-reload
 systemctl enable dispatch
-systemctl start dispatch
+systemctl restart dispatch
--- payment.sh
+++ payment.sh
@@ -1,5 +1,6 @@
 yum install python36 gcc python3-devel -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 cd /app
