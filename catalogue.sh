1f955fc92a477e41715ac6c6e653baf03f38b77c
madhavi <madhavilatha.008@gmail.com>
2023-08-12 13:32:31 -0500

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,43 +1,45 @@
 script=$(realpath "$0")
-script_path=$(dir_name $"Script")
+script_path=$(dir_name "$script")
 source ${script_path}/common.sh
 
+
+
 func_print_head "Configuring NodeJS Repos "
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 
 func_print_head "Install NodeJS"
-yum install nodejs -y
+yum install nodejs -y &>>$log_file
 
 func_print_head "Add Application User"
-useradd roboshop
+useradd roboshop &>>$log_file
 
 func_print_head "Create Application Directory"
-rm -rf /app
-mkdir /app
+rm -rf /app &>>$log_file
+mkdir /app &>>$log_file
 
 func_print_head "Download App Content "
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>$log_file
 cd /app
 
 func_print_head "Unzip App Content"
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>$log_file
 
 func_print_head" Add NodeJS Dependencies"
-npm install
+npm install &>>$log_file
 
 func_print_head "Copy Catalogue SystemD file"
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>$log_file
 
 func_print_head "Start Catalogue Service"
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
+systemctl daemon-reload &>>$log_file
+systemctl enable catalogue &>>$log_file
+systemctl restart catalogue &>>$log_file
 
 func_print_head "Copy Mongodb repo"
-cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
 
 func_print_head "install MongoDb Client"
-yum install mongodb-org-shell -y
+yum install mongodb-org-shell -y &>>$log_file
 
 func_print_head "Load Schema"
-mongo --host mongodb-dev.madhavi91.online </app/schema/catalogue.js
+mongo --host mongodb-dev.madhavi91.online </app/schema/catalogue.js &>>$log_file
