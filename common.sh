f425728203e872e022e05c3aaca6ffd507e634d9
madhavi <madhavilatha.008@gmail.com>
2023-08-12 12:28:58 -0500

    pipeline

--- common.sh
+++ common.sh
@@ -3,7 +3,7 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
- func_print_head() {
+func_print_head() {
   echo -e "\e[35m>>>>>>>>> $1 <<<<<<<<\e[0m"
   echo -e "\e[35m>>>>>>>>> $1 <<<<<<<<\e[0m"
   }
