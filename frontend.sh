ba0634a408be30335869ecf318476833352f283a
madhavi <madhavilatha.008@gmail.com>
2023-08-12 13:01:01 -0500

    common

--- frontend.sh
+++ frontend.sh
@@ -4,20 +4,26 @@ source ${script_path}/common.sh
 
 func_print_head "Install Nginx"
 yum install nginx -y
+func_stat_check $?
 
 func_print_head "Copy roboshop Config file"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+func_stat_check $?
 
 func_print_head "Clean Old App content"
 rm -rf /usr/share/nginx/html/*
+func_stat_check $?
 
 func_print_head "Download App Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+func_stat_check $?
 
 func_print_head "Extracting App Content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+func_stat_check $?
 
 func_print_head "Start Nginx"
 systemctl restart nginx
 systemctl enable nginx
+func_stat_check $?
