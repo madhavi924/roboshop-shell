9bad13dd3447c2021b3a32634150fc5323ba3af8
madhavi <madhavilatha.008@gmail.com>
2023-08-12 12:56:12 -0500

    common

--- common.sh
+++ common.sh
@@ -2,8 +2,7 @@ app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 
-
- func_print_head() {
+func_print_head() {
   echo -e "\e[35m>>>>>>>>> $1 <<<<<<<<\e[0m"
   echo -e "\e[35m>>>>>>>>> $1 <<<<<<<<\e[0m"
   }
